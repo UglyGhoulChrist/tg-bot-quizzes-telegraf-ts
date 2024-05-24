08839013f1725c9200fb0aab007bbcf2ca8b7527
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2024-05-25 01:30:04 +0300

    Added a random selection of answer and fact options

--- dist/data/data.js
+++ dist/data/data.js
@@ -1,21 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.data = void 0;
-const updateDataAnimals04_1 = require("./updateDataAnimals04");
+const dataAnimals04_1 = require("./dataAnimals04");
 const dataAnimals59_1 = require("./dataAnimals59");
 const dataAnimals1014_1 = require("./dataAnimals1014");
 const dataAnimals1519_1 = require("./dataAnimals1519");
-const dataSpaces04_1 = require("./dataSpaces04");
 const dataSpaces59_1 = require("./dataSpaces59");
+const dataSpaces04_1 = require("./dataSpaces04");
 const dataSpaces1014_1 = require("./dataSpaces1014");
 const dataSpaces1519_1 = require("./dataSpaces1519");
 exports.data = {
     animals: {
         description: 'Викторина про животных',
-        listQuestions: [...updateDataAnimals04_1.updateDataAnimals04, ...dataAnimals59_1.dataAnimals59, ...dataAnimals1014_1.dataAnimals1014, ...dataAnimals1519_1.dataAnimals1519,],
+        listQuestions: [...dataAnimals04_1.dataAnimals04, ...dataAnimals59_1.dataAnimals59, ...dataAnimals1014_1.dataAnimals1014, ...dataAnimals1519_1.dataAnimals1519,],
     },
     spaces: {
         description: 'Викторина про космос',
         listQuestions: [...dataSpaces04_1.dataSpaces04, ...dataSpaces59_1.dataSpaces59, ...dataSpaces1014_1.dataSpaces1014, ...dataSpaces1519_1.dataSpaces1519,],
-    },
+    }
 };
--- src/data/data.ts
+++ src/data/data.ts
@@ -1,13 +1,12 @@
 import { IData } from './interface.data'
-import { updateDataAnimals04 } from './updateDataAnimals04'
+import { dataAnimals04 } from './dataAnimals04'
 import { dataAnimals59 } from './dataAnimals59'
 import { dataAnimals1014 } from './dataAnimals1014'
 import { dataAnimals1519 } from './dataAnimals1519'
-import { dataSpaces04 } from './dataSpaces04'
 import { dataSpaces59 } from './dataSpaces59'
+import { dataSpaces04 } from './dataSpaces04'
 import { dataSpaces1014 } from './dataSpaces1014'
 import { dataSpaces1519 } from './dataSpaces1519'
-
 
 // Объект викторин, хранящий все викторины
 // Ключом является строка, представляющая категорию викторины,
@@ -15,10 +14,10 @@ import { dataSpaces1519 } from './dataSpaces1519'
 export const data: IData = {
     animals: {
         description: 'Викторина про животных',
-        listQuestions: [...updateDataAnimals04, ...dataAnimals59, ...dataAnimals1014, ...dataAnimals1519,],
+        listQuestions: [...dataAnimals04, ...dataAnimals59, ...dataAnimals1014, ...dataAnimals1519,],
     },
     spaces: {
         description: 'Викторина про космос',
         listQuestions: [...dataSpaces04, ...dataSpaces59, ...dataSpaces1014, ...dataSpaces1519,],
-    },
+    }
 }
