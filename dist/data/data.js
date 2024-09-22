5c8e0f6365ffde790b935fd85a660315d2492419
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2024-09-22 05:30:21 +0300

    feat(data): The nature category has been created and 20 questions have been added

--- dist/data/data.js
+++ dist/data/data.js
@@ -4,17 +4,22 @@ exports.data = void 0;
 const dataAnimals_1 = require("./dataAnimals");
 const dataSpaces_1 = require("./dataSpaces");
 const dataInsects_1 = require("./dataInsects");
+const dataNature_1 = require("./dataNature");
 exports.data = {
+    nature: {
+        description: "Викторина про природу",
+        listQuestions: dataNature_1.dataNature,
+    },
     animals: {
-        description: 'Викторина про животных',
+        description: "Викторина про животных",
         listQuestions: dataAnimals_1.dataAnimals,
     },
+    insects: {
+        description: "Викторина про маленьких обитателей природы",
+        listQuestions: dataInsects_1.dataInsects,
+    },
     spaces: {
-        description: 'Викторина про космос',
+        description: "Викторина про космос",
         listQuestions: dataSpaces_1.dataSpaces,
     },
-    insects: {
-        description: 'Викторина про насекомых и маленьких обитателей природы',
-        listQuestions: dataInsects_1.dataInsects,
-    }
 };
--- src/data/data.ts
+++ src/data/data.ts
@@ -1,23 +1,27 @@
-import { IData } from './interface.data'
-import { dataAnimals } from './dataAnimals'
-import { dataSpaces } from './dataSpaces'
-import { dataInsects } from './dataInsects'
+import { IData } from "./interface.data";
+import { dataAnimals } from "./dataAnimals";
+import { dataSpaces } from "./dataSpaces";
+import { dataInsects } from "./dataInsects";
+import { dataNature } from "./dataNature";
 
 // Объект викторин, хранящий все викторины
 // Ключом является строка, представляющая категорию викторины,
 // значением - описание викторины и список вопросов
 export const data: IData = {
+    nature: {
+        description: "Викторина про природу",
+        listQuestions: dataNature,
+    },
     animals: {
-        description: 'Викторина про животных',
+        description: "Викторина про животных",
         listQuestions: dataAnimals,
     },
-    spaces: {
-        description: 'Викторина про космос',
-        listQuestions: dataSpaces,
-
-    },
     insects: {
-        description: 'Викторина про насекомых и маленьких обитателей природы',
+        description: "Викторина про маленьких обитателей природы",
         listQuestions: dataInsects,
-    }
-}
+    },
+    spaces: {
+        description: "Викторина про космос",
+        listQuestions: dataSpaces,
+    },
+};
