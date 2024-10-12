dd3b8e4daf14d6154b99631dd62c9b66a38689b9
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2024-10-12 22:27:19 +0300

    chore: Deleted tsc-watch, added rollup

--- dist/commands/commands.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.commands = void 0;
-exports.commands = [
-    { command: 'help', description: 'Помощь' },
-    { command: 'start', description: 'Старт' },
-    { command: 'reset', description: 'Сброс' },
-    { command: 'statistics', description: 'Статистика' },
-];
---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import typescript from "@rollup/plugin-typescript"
+
+export default {
+    input: "src/quizzes.ts", // точка входа
+    output: {
+        // файл бандла
+        file: "dist/quizzes.js",
+        format: "es", // формат модуля (cjs для Node.js)
+    },
+    plugins: [
+        typescript(), // плагин для работы с TypeScript
+    ],
+    external: ["dotenv", "telegraf", "node:fs/promises", "node:path"],
+    watch: {
+        include: ["src/**"],
+        exclude: ["dist/**", "*.log", "*.json"],
+    },
+}
