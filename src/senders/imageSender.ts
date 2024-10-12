d0d2c9c527bc12d2ebefae615de0705bce138101
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2024-10-13 00:45:42 +0300

    fix: Fix the import of access, mkdir, readFile, writeFile

--- src/senders/imageSender.ts
+++ src/senders/imageSender.ts
@@ -1,20 +1,34 @@
 import { Context, Telegraf } from "telegraf";
-import { promises as fsPromises } from 'node:fs';
-import { join } from 'node:path';
-import { getUserState } from '../states/userStates';
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+import { getUserState } from "../states/userStates";
 import { appendError } from "../loggers/appendError";
 import { delay } from "../utils/delay";
 
-export async function imageSender(bot: Telegraf<Context>, userId: number): Promise<void> {
-    const { chatId, currentListQuestions, currentIndexQuestion, currentCategory } = getUserState(userId);
+export async function imageSender(
+    bot: Telegraf<Context>,
+    userId: number,
+): Promise<void> {
+    const {
+        chatId,
+        currentListQuestions,
+        currentIndexQuestion,
+        currentCategory,
+    } = getUserState(userId);
     const { image } = currentListQuestions[currentIndexQuestion];
 
     try {
-        const photoPath: string = join(__dirname, '..', 'images', currentCategory, image);
-        const photo: Buffer = await fsPromises.readFile(photoPath);
+        const photoPath: string = join(
+            __dirname,
+            "..",
+            "images",
+            currentCategory,
+            image,
+        );
+        const photo: Buffer = await readFile(photoPath);
         await bot.telegram.sendPhoto(chatId, { source: photo });
         // Задержка в 3 секунды после отправки картинки
-        await delay(3000)
+        await delay(3000);
     } catch (error) {
         appendError(error as NodeJS.ErrnoException);
     }
