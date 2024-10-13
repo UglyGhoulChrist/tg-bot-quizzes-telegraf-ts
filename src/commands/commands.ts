a6376acf4ba2abafc83fcab8bbc3fe5803385c8c
UglyGhoulChrist <UglyGhoulChrist@gmail.com>
2024-10-13 18:35:25 +0300

    refactor(src/commands): Refactored the code

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,9 +1,9 @@
-import { BotCommand } from '@telegraf/types'
+import { BotCommand } from "@telegraf/types";
 
 // Основные команды: `/help`, `/start`, `/reset`, `/statistics`
 export const commands: BotCommand[] = [
-    { command: 'help', description: 'Помощь' },
-    { command: 'start', description: 'Старт' },
-    { command: 'reset', description: 'Сброс' },
-    { command: 'statistics', description: 'Статистика' },
-]
+    { command: "help", description: "Помощь" },
+    { command: "start", description: "Старт" },
+    { command: "reset", description: "Сброс" },
+    { command: "statistics", description: "Статистика" },
+];
